376f8fcb1836a480a0930f74881bc41129639948
developerLee <alexxlee66@gmail.com>
2021-05-11 21:34:22 +0900

    Update Tensile Strength, Elongation, Yield Strength

--- tensile test/tensile_test_Youngs_Modulus.py
+++ tensile test/tensile_test_Youngs_Modulus.py
@@ -1,4 +1,4 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 import math
 
@@ -11,7 +11,7 @@ getcontext().prec = 20
 def _gradient(x_1, x_2, y_1, y_2):
    return (Decimal(y_2)-Decimal(y_1))/(Decimal(x_2)-Decimal(x_1))
 
-excel_data = pd.read_excel("C:\\Program Files\\Automation Anywhere\\Bot Agent\\tesnile test\\stress_strain1.xlsx")
+excel_data = pd.read_excel("C:\\Program Files\\Automation Anywhere\\Bot Agent\\tensile test\\stress_strain1.xlsx")
 strain_list = excel_data['strain'].tolist()
 stress_list = excel_data['stress'].tolist()
 
@@ -22,10 +22,10 @@ for i in range(n_samples):
    if strain_list[i] > 0.02:
       break
    else:
-      g_sample = _gradient(strain_list[i], strain_list[i+10], stress_list[i], stress_list[i+10])
+      g_sample = _gradient(strain_list[i], strain_list[i+5], stress_list[i], stress_list[i+5])
       if g_sample <= 0:
          continue
-      else:
+      else: 
          g_list.append(g_sample)
 
 n_g_samples = len(g_list)
@@ -77,6 +77,8 @@ for x,y in zip(strain_list, stress_list):
    distance.append(abs(final_gradient*x+(-1)*y+(-1)*final_gradient*0.002)/math.sqrt(final_gradient**2 + 1))
 
 intersection = [coordinates[0][distance.index(min(distance))], coordinates[1][distance.index(min(distance))],]
+TS = [coordinates[0][stress_list.index(max(stress_list))], max(stress_list)]
+elongation_point = [max(strain_list), coordinates[1][strain_list.index(max(strain_list))]]
 
 def gradient():
    return final_gradient
@@ -84,17 +86,24 @@ def gradient():
 def _intersection():
    return intersection
 
-# if __name__ == '__main__':
-#    print(_intersection())
-   # print(final_gradient)
-   # # print(distance)
-   # print(intersection[0], intersection[1])
-   # start = [0, 0.01]
-   # end = [0, final_gradient*0.01]
-   # g_start = [0.002, 0.012]
-   # g_end = [0, final_gradient*0.01]
-   # plt.plot(strain_list, stress_list)
-   # plt.plot(start, end)
-   # plt.plot(g_start, g_end)
-   # plt.plot(intersection[0], intersection[1], marker='o', markersize=5)
-   # plt.show()
+if __name__ == '__main__':
+   # print(_intersection())
+   # print(distance)
+   print("Young's Modulus: " + str("%.1f" %(final_gradient/1000)) + 'GPa')
+   print("Yield Strength: " + str(intersection[1]) + 'MPa')
+   print("Tensile Strength: " + str(TS[1]) + 'MPa')
+   print("Elongation: " + str(elongation_point[0]))
+   start = [0, 0.01]
+   end = [0, final_gradient*0.01]
+   g_start = [0.002, 0.012]
+   g_end = [0, final_gradient*0.01]
+   plt.plot(strain_list, stress_list)
+   plt.plot(start, end)
+   plt.plot(g_start, g_end)
+   plt.plot(intersection[0], intersection[1], marker='o', markersize=4, label='Yield Strength: ' + str("%.1f" %intersection[1]) + ' MPa')
+   plt.plot(TS[0], TS[1], marker='o', markersize=4, label='Tensile Strength: ' + str("%.1f" %TS[1]) + ' MPa')
+   plt.plot(elongation_point[0], elongation_point[1], marker='o', markersize=4, label='Elongation Point: ' + str(elongation_point[0]))
+   plt.legend()
+   plt.savefig('Stress Strain.png', dpi=600)
+   plt.show()
+   print("Done!")
